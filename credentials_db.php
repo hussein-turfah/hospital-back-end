42a4d68ef323f5858faaf6dd626426b3dccd557f
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-10 14:05:51 +0200

    Added credentials file for accessing the database.

---
+++ credentials_db.php
@@ -0,0 +1,13 @@
+<?php
+$host_name = 'localhost';
+$database = 'hospital_db';
+$username = 'root';
+$password = '';
+
+$link = new mysqli($host_name, $username, $password, $database);
+
+header("Access-Control-Allow-Origin: * ");
+header("Access-Control-Allow-Headers:*");
+if ($link->connect_error) {
+    die('<p>Failed to connect to MySQL: ' . $link->connect_error . '</p>');
+}
