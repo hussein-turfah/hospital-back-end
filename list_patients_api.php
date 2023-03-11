71e3cbac4dc6daa97c5d38615c5110d046b961a4
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-11 21:55:49 +0200

    Edited patients api to list the name with the id

--- list_patients_api.php
+++ list_patients_api.php
@@ -2,7 +2,7 @@
 
 include('credentials_db.php');
 
-$sql_statement = 'select id from users where usertype_id = 3';
+$sql_statement = 'select id,name from users where usertype_id = 3';
 
 $sql_result = mysqli_query($link,$sql_statement);
 $patients_data = mysqli_fetch_all($sql_result);
