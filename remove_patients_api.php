6e27fdcb2a9f1dfc74b5d4f1788b903b9844eff4
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-12 15:05:54 +0200

    Added an removing patients from hospital_users

--- remove_patients_api.php
+++ remove_patients_api.php
@@ -3,26 +3,26 @@ include('credentials_db.php');
 
 if ($_SERVER['REQUEST_METHOD'] =='POST'){
 
-  $user_id = $_POST['user_id'];
   $hospital_id = $_POST['hospital_id'];
+  $user_id = $_POST['user_id'];
 
-  $sql_statement = "insert into 
-  hospital_users(hospital_id,user_id,is_active) values(?,?,1)";
-  
+  $sql_statement = 'update hospital_users
+  set is_active = 0
+  where hospital_id = ? and user_id = ?';
+
   $sql_statement = mysqli_prepare($link,$sql_statement);
-  mysqli_stmt_bind_param($sql_statement, 'ii',$hospital_id,$user_id);
+  mysqli_stmt_bind_param($sql_statement,'ii',$hospital_id,$user_id);
+  
   if(mysqli_stmt_execute($sql_statement)){
-
-    $response['sucess'] = 'Patient sucessfully assigned.';
+    $response['sucess'] = 'Patient sucessfully removed.';
     echo json_encode($response);
   }else{
-    $error = 'There was an error assigning this patient!';
+    $error = 'There was an error removing this patient!';
     $response = ['response' => $error];
     echo json_encode([$error]);
-  }
+  };
 
 }
-
 
 
 
