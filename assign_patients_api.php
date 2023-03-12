3f6e872cbd632e1b08b48796dea6ba06bc17d938
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-12 15:42:49 +0200

    Added a code for checking if the patient is already in a hospital before assigning him.

--- assign_patients_api.php
+++ assign_patients_api.php
@@ -6,21 +6,39 @@ if ($_SERVER['REQUEST_METHOD'] =='POST'){
   $user_id = $_POST['user_id'];
   $hospital_id = $_POST['hospital_id'];
 
-  $sql_statement = "insert into 
-  hospital_users(hospital_id,user_id,is_active) values(?,?,1)";
-  
-  $sql_statement = mysqli_prepare($link,$sql_statement);
-  mysqli_stmt_bind_param($sql_statement, 'ii',$hospital_id,$user_id);
-  if(mysqli_stmt_execute($sql_statement)){
+  $check_presence = 'SELECT is_active 
+  from hospital_users
+  where user_id = ?
+  and is_active = 1';
 
-    $response['sucess'] = 'Patient sucessfully assigned.';
-    echo json_encode($response);
+  $check_presence = mysqli_prepare($link,$check_presence);
+  mysqli_stmt_bind_param($check_presence,'i',$user_id);
+
+  mysqli_stmt_execute($check_presence);
+  $presence_result = mysqli_stmt_get_result($check_presence);
+  $presence_data = mysqli_fetch_all($presence_result);
+
+  //this if statement checks if the patient is not present in any hospital.
+  if(count($presence_data)==0){
+    $sql_statement = "insert into 
+    hospital_users(hospital_id,user_id,is_active) values(?,?,1)";
+    
+    $sql_statement = mysqli_prepare($link,$sql_statement);
+    mysqli_stmt_bind_param($sql_statement, 'ii',$hospital_id,$user_id);
+    if(mysqli_stmt_execute($sql_statement)){
+
+      $response['sucess'] = 'Patient sucessfully assigned.';
+      echo json_encode($response);
+    }else{
+      $error = 'There was an error assigning this patient!';
+      $response = ['response' => $error];
+      echo json_encode([$error]);
+    }
   }else{
-    $error = 'There was an error assigning this patient!';
+    $error = 'This patient is already in a hospital!';
     $response = ['response' => $error];
     echo json_encode([$error]);
-  }
-
+  };
 }
 
 
